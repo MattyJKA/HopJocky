5866417e16b388cfb1c031ddff55127c8ee4adab
Matt Union <matt2union@utexas.edu>
2016-04-06 14:39:41 -0500

    Vihn's map work merged with Matt's work

--- HopJocky/MapViewController.swift
+++ HopJocky/MapViewController.swift
@@ -9,30 +9,27 @@
 import UIKit
 import GoogleMaps
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var viewMap: GMSMapView!
+    
+    @IBOutlet weak var searchBarTextField: UITextField!
+    
+    var locationManager = CLLocationManager()
+    
+    var mapTasks = MapTasks()
+    
+    var locationMarker: GMSMarker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let camera = GMSCameraPosition.cameraWithLatitude(30.295561, longitude: -97.743356, zoom: 12)
-        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        mapView.myLocationEnabled = true
-        mapView.settings.compassButton = true
-    
-        self.view = mapView
+        let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(48.857165, longitude: 2.354613, zoom: 12.0)
+        viewMap.camera = camera
         
-        let marker = GMSMarker()
-        let marker1 = GMSMarker()
+        locationManager.delegate = self
         
-        marker.position = CLLocationCoordinate2DMake(30.295561, -97.743356)
-        marker.title = "Growler USA"
-        marker.snippet = "609 W. 29th St, Austin, TX 78705"
-        marker.map = mapView
-        
-        marker1.position = CLLocationCoordinate2DMake(30.266132, -97.745825)
-        marker1.title = "The Ginger Man"
-        marker1.snippet = "301 Lavaca St, Austin, TX 78701"
-        marker1.map = mapView
+        locationManager.requestWhenInUseAuthorization()
         
         
         
@@ -43,7 +40,89 @@ class MapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func searchAddress(sender: AnyObject) {
+        
+        let address = searchBarTextField.text!
+        
+        self.mapTasks.geocodeAddress(address, withCompletionHandler: { (status, success) -> Void in
+            
+            if !success {
+                print(status)
+                
+                if status == "ZERO_RESULTS" {
+                    self.showAlertWithMessage("The location could not be found.")
+                }
+            }
+            else {
+                let coordinate = CLLocationCoordinate2D(latitude: self.mapTasks.fetchedAddressLatitude, longitude: self.mapTasks.fetchedAddressLongitude)
+                
+                self.viewMap.camera = GMSCameraPosition.cameraWithTarget(coordinate, zoom: 15.0)
+                
+                self.setupLocationMarker(coordinate)
+                
+            }
+            
+        })
+        
+    }
 
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        
+        if status == .AuthorizedWhenInUse {
+            
+            locationManager.startUpdatingLocation()
+            
+            viewMap.myLocationEnabled = true
+            
+            viewMap.settings.myLocationButton = true
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            
+            viewMap.camera = GMSCameraPosition(target: location.coordinate, zoom: 12, bearing: 0, viewingAngle: 0)
+            
+            locationManager.stopUpdatingLocation()
+        }
+        
+    }
+    
+    func showAlertWithMessage(message: String) {
+        let alertController = UIAlertController(title: "HopJocky", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let closeAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.Cancel) { (alertAction) -> Void in
+            
+        }
+        
+        alertController.addAction(closeAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    
+    func setupLocationMarker(coordinate: CLLocationCoordinate2D) {
+        
+        if locationMarker != nil {
+            locationMarker.map = nil
+        }
+        
+        locationMarker = GMSMarker(position: coordinate)
+        
+        locationMarker.map = viewMap
+        
+        locationMarker.title = mapTasks.fetchedFormattedAddress
+        //        locationMarker.icon = GMSMarker.markerImageWithColor(UIColor.blueColor())
+        locationMarker.appearAnimation = kGMSMarkerAnimationPop
+        locationMarker.opacity = 0.6
+//        locationMarker.flat = true
+    }
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
